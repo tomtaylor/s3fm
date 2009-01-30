340e2ea033c41fadd08f81bf2f1598ff0ad4e9df
Tom Taylor <tom@mu.local>
2009-01-30 23:52:33 +0000

    Oh yes. It fucking does everything now.

--- playlist.rb
+++ playlist.rb
@@ -6,15 +6,19 @@ require 'open-uri'
 
 class Playlist
   
-  attr_reader :mp3s, :title
+  attr_reader :mp3s
   
   def initialize(bucket)
-    @bucket, @title = bucket
+    @bucket = bucket
     uri = URI.parse("https://#{bucket}.s3.amazonaws.com/")
     
     doc = Hpricot::XML(open(uri))
     all_keys = doc.search('/ListBucketResult/Contents/Key').map{ |k| k.inner_text.strip }
     @mp3s = all_keys.select{ |k| k =~ /.mp3$/i }.map { |m| uri.to_s + m }
+  end
+  
+  def title
+    @bucket
   end
   
 end
--- public/application.js
+++ public/application.js
@@ -3,30 +3,59 @@ soundManager.flashVersion = 9;
 soundManager.consoleOnly = true;
 
 soundManager.onload = function() {
-
-  $.getJSON('/radiotom?format=json', function(data) {
-    
-    // ensures it loops if this sound is the last one
-    function playNext(i) {
-      if ((i+1) == data.length) {
-        soundManager.play('0');
-      } else {
-        soundManager.play((i+1).toString());
-      }
+  
+  var currentSound = 0;
+  var soundsCount = 0;
+  
+  $('#playpause-button').click(function() {
+    if ($(this).hasClass("playing")) {
+      soundManager.pauseAll();
+      $(this).text("Play");
+    } else {
+      soundManager.resumeAll();
+      $(this).text("Pause");
     }
+    $(this).toggleClass("playing");
+    return false;
+  });
+  
+  $('#next-button').click(function() {
+    soundManager.stopAll();
+    playNext();
+    return false;
+  });
+  
+  
+  
+  // ensures it loops if this sound is the last one
+  function playNext(i) {
+    if ((currentSound + 1) == soundsCount) {
+      soundManager.play('0');
+    } else {
+      soundManager.play((currentSound + 1).toString());
+    }
+  }
+  
+
+  $.getJSON('/speechificationaudio?format=json', function(data) {
+    
+    soundsCount = data.length;
     
     $.each(data, function(i, item) {
       var sound = soundManager.createSound({
         id: i.toString(),
         url: item,
+        onplay: function() {
+          currentSound = i;
+        },
         onfinish: function() {
-          playNext(i);
+          playNext();
         },
         onload: function() {
           // if there's a load failure, unload and play the next one
           if (this.readyState == 2) {
             this.unload();
-            playNext(i);
+            playNext();
           }
         }
       });
